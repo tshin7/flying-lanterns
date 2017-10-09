1ae11f76b08a43a5dd7ccfdc62544902e5963788
Ted Shin <tdyshin@gmail.com>
2017-10-09 18:35:01 -0400

    Add footer

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 // import './App.css';
 
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
+
+        <Footer />
       </div>
     );
   }
