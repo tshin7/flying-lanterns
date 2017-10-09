607c5c999424bb441656e03019b56df2d2a1c1b2
Ted Shin <tdyshin@gmail.com>
2017-10-09 16:07:42 -0400

    Set up nav bar

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
+
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>flying-lanterns</h1>
+        <NavBar />
       </div>
     );
   }
