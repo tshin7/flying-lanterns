e0e66688eb4af40b1e562908ddefc6812a560505
Ted Shin <tdyshin@gmail.com>
2017-10-21 23:44:54 -0400

    Set up browserrouter and main.js

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import './App.css';
 
 import NavBar from './components/NavBar';
+import Main from './components/Main';
 import Footer from './components/Footer';
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
-
+        <Main />
         <Footer />
       </div>
     );
