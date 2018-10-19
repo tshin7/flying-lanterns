b80fa520b14e50e00e15c469145f7478c194db94
Theodore Shin <tdyshin@gmail.com>
2018-10-19 15:57:39 -0400

    Clean up Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
 import {
-  Button,
   Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Responsive,
   Segment,
-  Sidebar,
-  Visibility,
 } from 'semantic-ui-react'
 
 export default class Footer extends Component {
