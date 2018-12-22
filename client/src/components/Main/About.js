6c46e3508b606d58caf8bb35162f5eba53166201
Theodore Shin <tdyshin@gmail.com>
2018-12-22 15:26:50 -0500

    Set up faq section in about. Resolves #11

--- client/src/components/Main/About.js
+++ client/src/components/Main/About.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import { MainDiv } from './Main.style.js';
+import {
+  Container,
+  Divider,
+  Header,
+  Segment,
+} from 'semantic-ui-react'
 
 export default class About extends Component {
   render() {
     return (
       <MainDiv>
-        Flying Lanterns provides costumes and accessories for cosplaying.
-        At the moment. everything on this site is created by Misha.
+        <Segment vertical>
+          <Container text>
+            Flying Lanterns provides costumes and accessories for cosplaying.
+            At the moment. everything on this site is created by Misha.
+          </Container>
+        </Segment>
+
+        <Segment style={{ padding: '2em 0em' }} vertical>
+        <Container>
+          <Header as='h3' style={{ fontSize: '2em' }}>
+            Frequently Asked Questions
+          </Header>
+          <Divider />
+          <p style={{ fontSize: '1.33em' }}>
+            How do I order?
+          </p>
+          <p style={{ fontSize: '1em' }}>
+            Add to cart and process payment.
+          </p>
+        </Container>
+        </Segment>
       </MainDiv>
     );
   }
