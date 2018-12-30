0554a9cd13b8d889adb0eea2c720431b5a746bfa
Theodore Shin <tdyshin@gmail.com>
2018-12-30 17:42:30 -0500

    Fix lint errors in Main.style.js

--- client/src/components/Main/Main.style.js
+++ client/src/components/Main/Main.style.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
-export const MainDiv = styled.div`
+const MainDiv = styled.div`
   padding-top: 4em;
   text-align: center;
-
 `;
+
+export default MainDiv;
