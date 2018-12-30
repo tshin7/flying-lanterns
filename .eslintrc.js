66761490f05f59275c7897afcf6d4a66719f782c
Theodore Shin <tdyshin@gmail.com>
2018-12-29 19:05:21 -0500

    Fix lint errors in Signup.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "node": true
+  },
   "rules": {
     'no-console': 'off',
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
   },
 };
--- client/src/components/Main/Signup.js
+++ client/src/components/Main/Signup.js
@@ -1,44 +1,46 @@
-import React, { Component } from 'react';
-import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
-import { MainDiv } from './Main.style.js';
+import React from 'react';
+import {
+  Button, Form, Grid, Header, Message, Segment,
+} from 'semantic-ui-react';
+import { MainDiv } from './Main.style';
 
-export default class Signup extends Component {
-  render() {
-    return (
-      <MainDiv>
-        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as='h2' color='teal' textAlign='center'>
-              Sign up for a new account
-            </Header>
-            <Form size='large'>
-              <Segment stacked>
-                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-                <Form.Input
-                  fluid
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='Password'
-                  type='password'
-                />
-                <Form.Input
-                  fluid
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='Repeat Password'
-                  type='password'
-                />
-                <Button color='teal' fluid size='large'>
-                  Sign Up
-                </Button>
-              </Segment>
-            </Form>
-            <Message>
-              Already have an account? <a href='/login'>Login</a>
-            </Message>
-          </Grid.Column>
-        </Grid>
-      </MainDiv>
-    );
-  }
-}
+const Signup = () => (
+  <MainDiv>
+    <Grid textAlign="center" style={{ height: '100%' }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          Sign up for a new account
+        </Header>
+        <Form size="large">
+          <Segment stacked>
+            <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail address" />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Repeat Password"
+              type="password"
+            />
+            <Button color="teal" fluid size="large">
+              Sign Up
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          Already have an account?
+          {' '}
+          <a href="/login">Login</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  </MainDiv>
+);
+
+export default Signup;
