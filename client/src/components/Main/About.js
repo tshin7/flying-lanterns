0f2a37a95fe11d292c42fa9a102a2c72b588a367
Theodore Shin <tdyshin@gmail.com>
2018-12-30 22:51:15 -0500

    Fix lint errors in About.js

--- client/src/components/Main/About.js
+++ client/src/components/Main/About.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
-import { MainDiv } from './Main.style.js';
+import React from 'react';
 import {
   Container,
   Divider,
   Header,
   Segment,
-} from 'semantic-ui-react'
+} from 'semantic-ui-react';
+import { MainDiv } from './Main.style';
 
-export default class About extends Component {
-  render() {
-    return (
-      <MainDiv>
-        <Segment vertical>
-          <Container text>
-            Flying Lanterns provides costumes and accessories for cosplaying.
-            At the moment. everything on this site is created by Misha.
-          </Container>
-        </Segment>
+const About = () => (
+  <MainDiv>
+    <Segment vertical>
+      <Container text>
+        Flying Lanterns provides costumes and accessories for cosplaying.
+        At the moment. everything on this site is created by Misha.
+      </Container>
+    </Segment>
 
-        <Segment style={{ padding: '2em 0em' }} vertical>
-        <Container>
-          <Header as='h3' style={{ fontSize: '2em' }}>
-            Frequently Asked Questions
-          </Header>
-          <Divider />
-          <p style={{ fontSize: '1.33em' }}>
-            How do I order?
-          </p>
-          <p style={{ fontSize: '1em' }}>
-            Add to cart and process payment.
-          </p>
-        </Container>
-        </Segment>
-      </MainDiv>
-    );
-  }
-}
+    <Segment style={{ padding: '2em 0em' }} vertical>
+      <Container>
+        <Header as="h3" style={{ fontSize: '2em' }}>
+          Frequently Asked Questions
+        </Header>
+        <Divider />
+        <p style={{ fontSize: '1.33em' }}>
+          How do I order?
+        </p>
+        <p style={{ fontSize: '1em' }}>
+          Add to cart and process payment.
+        </p>
+      </Container>
+    </Segment>
+  </MainDiv>
+);
+
+export default About;
