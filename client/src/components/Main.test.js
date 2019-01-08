7370d8a2f06a6bda413a5707cee002d58f5a1c4d
Theodore Shin <tdyshin@gmail.com>
2019-01-07 23:49:24 -0500

    Add test for Main.js

--- client/src/components/Main.test.js
+++ client/src/components/Main.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Navbar from './Navbar';
+import Main from './Main';
 
-describe('Navbar', () => {
+describe('Main', () => {
   it('should render correctly with no props', () => {
-    const component = shallow(<Navbar />);
+    const component = shallow(<Main />);
 
     expect(component).toMatchSnapshot();
   });
